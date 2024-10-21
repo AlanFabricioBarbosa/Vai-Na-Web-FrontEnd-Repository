7115742ecdee8345f773bb5f64650519d7415dd3
Alan Fabrício <alanfabriciobarbosa1@gmail.com>
2024-10-21 10:38:41 -0300

    :sparkles: Introdução ao Javascript aula 21-10-2024

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log("Olá Mundo!");
+console.log(3 * 5);
+
+// Tipo String -> texto
+var aluno = "Alan";
+
+// Tipo Number -> numeros
+let idade = 26;
+idade = 20;
+
+// Tipo boolean -> Tipo que trabalha apenas usando True (verdadeiro) ou false (falso)
+let nome = true;
+
+// Tipos nulos
+let professor = null;
+
+// Tipo indefinido
+let dia;
+dia = 20;
+
+console.log(
+  "|" + aluno + "|" + idade + "|" + nome + "|" + professor + "|" + dia
+);
