84719cd952a6c8eb25235e9327c8b184312fd1e9
Alan Fabrício <alanfabriciobarbosa1@gmail.com>
2024-10-25 10:57:04 -0300

    :heavy_check_mark: Resumo das aulas de javascript e pequeno desafio em grupo

--- index.js
+++ index.js
@@ -1,90 +1,43 @@
-// var -> Não utilizamos mais
+let verificaDia = false;
 
-// Variavel que pode ser reatribuida
-let nome = "Alan";
-nome = "Vinicius professor"; // <- String - valor do tipo texto
+console.log(verificaDia === true ? "Está de dia" : "Está de noite");
 
-console.log(nome);
+let verificaTemperatura = 30;
 
-// Variavel que não pode ser reatribuida
-const rg = 12562536; // <- number - valor do tipo number
+console.log(
+  verificaTemperatura > 30
+    ? "Está muito quente"
+    : "A temperatura está agradável"
+);
 
-console.log(rg);
+let verificaIdade = 26;
 
-let dia = true; // <- Boolean - valor  de verdadeiro (true) ou falso (false)
-
-let aluno; // <- Tipo undefined
-
-let noite = null; // <- Tipo nulo
-
-console.log(5 / "y"); // <- Resultado not a number
-
-//Operadores de comparação:
-
-/*
-  Atribuição de valor -> =
-  Compara se valores são iguais -> ==
-  Compara valores e tipos -> ===
-  Compara se os valores são diferentes -> !=
-  Compara se os valores são estritamente diferentes -> !==
-  Compara se o valor é maior que outro -> >
-  Compara se o valor é menor que outro -> <
-*/
-
-let number = 2;
-let numero = "2";
-
-console.log("Compara se valores são iguais");
-console.log(number == numero);
-console.log("Compara valores e tipos");
-console.log(number === numero);
-console.log("Compara se os valores são diferentes");
-console.log(number != numero);
-console.log("Compara se os valores são estritamente diferentes");
-console.log(number !== numero);
-console.log("Compara se o valor é maior que outro");
-console.log(3 > 2);
-console.log("Compara se o valor é maior que outro");
-console.log(2 < 3);
-console.log("Compara se o valor é maior e igual que outro");
-console.log(3 >= 2);
-console.log("Compara se o valor é menor e igual que outro");
-console.log(2 <= 3);
-
-/*
-  if(condição) {
-    retorno da condição
-  }
-*/
-
-console.log("Verificando condicional");
-
-let idade = 15;
-
-if (idade > 18) {
-  console.log("Você é maior de idade");
-} else if (idade == 15) {
-  console.log("Está na idade certa");
+if (verificaIdade < 12) {
+  console.log("Criança");
+} else if (verificaIdade >= 12 && verificaIdade <= 18) {
+  console.log("Adolescente");
+} else if (verificaIdade >= 19 && verificaIdade <= 60) {
+  console.log("Adulto");
 } else {
-  console.log("Error");
+  console.log("Idoso");
 }
 
-// Condicional com ternario
-console.log(idade > 18 ? "Você é maior de idade" : "Você é menor de idade");
+let calcNotas = 8.5;
 
-let temluz = false;
-let sim = "Acenda as luzes";
-let nao = "Aguarde a energia voltar";
-
-if (temluz) {
-  console.log(sim);
-} else {
-  console.log(nao);
+switch (true) {
+  case calcNotas >= 9:
+    console.log("Nota A");
+    break;
+  case calcNotas >= 8:
+    console.log("Nota B");
+    break;
+  case calcNotas >= 7:
+    console.log("Nota C");
+    break;
+  case calcNotas >= 6:
+    console.log("Nota D");
+    break;
+  default:
+    console.log("Nota F");
+    break;
 }
-
-// Condicional com ternario
-console.log(temluz == true ? sim : nao);
-
-let valor = prompt("Digite um numero!");
-
-valor == 5 ? alert("Você foi premiado") : alert("Você errou");
