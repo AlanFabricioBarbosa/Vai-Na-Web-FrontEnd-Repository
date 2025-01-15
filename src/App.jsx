ba9a717b1c059954ab50e321e1ad728f9b7523e7
Alan Fabrício <alanfabriciobarbosa1@gmail.com>
2025-01-15 10:31:19 -0300

    :sparkles: feat: Aula de React Hook 15/01/2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
 import "./global.scss";
+import { useState } from "react";
 
 export default function App() {
+  const [name, setName] = useState("joe");
+  const [color, setColor] = useState("#F00");
+  const [num, setNum] = useState(0);
+
+  const handleName = () => {
+    setName("Alan");
+  };
+
+  const handleColor = () => {
+    setColor("#0F0");
+  };
+
+  const handleNum = () => {
+    setNum(num + 1);
+  };
+
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <section style={{ background: color }}>
+        <h1>Meu nome é: {name}</h1>
+        <h2>Você recebeu {num} de likes</h2>
+        <button onClick={handleName}>Botão setName{handleName}</button>
+        <button onClick={handleColor}>Mudar a cor</button>
+        <button onClick={handleNum}>Dar like</button>
+      </section>
     </>
   );
 }
--- src/components/Main/index.jsx
+++
@@ -1,5 +0,0 @@
-import S from "../style.module.scss";
-
-export default function Main() {
-  return <main>Main</main>;
-}
